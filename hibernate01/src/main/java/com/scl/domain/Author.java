4d1f5d2d623a8f970d3958649cd3f478bf3300e5
shchlu188@163.com <chenglu8295>
2019-11-20 22:26:40 +0800

    hibernate01

---
+++ hibernate01/src/main/java/com/scl/domain/Author.java
@@ -0,0 +1,47 @@
+package com.scl.domain;
+
+import javax.persistence.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chenglu
+ * Date: 2019/11/20
+ * Description:
+ */
+@Entity
+@Table(name = "t_author")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "author_name")
+    private String name;
+
+    @Column(name = "author_age")
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
---
+++ hibernate01/src/main/java/com/scl/domain/Book.java
@@ -0,0 +1,59 @@
+package com.scl.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chenglu
+ * Date: 2019/11/20
+ * Description: 图书
+ */
+public class Book {
+    private Integer id;
+
+    private String name;
+
+    private String author;
+
+    private double price;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
